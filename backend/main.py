b7fd27d0e9600b60122b01131e452d76381c64f1
Adar Schwarzbach <adar.schwarzbach@duke.edu>
2022-05-26 12:36:25 -0700

    restart

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,24 @@
-from flask import Flask, jsonify
-import json
-import time
+from flask import Flask, jsonify, make_response
+
+
+# main = Blueprint('main',__name__)
+
+# @main.route('/get_odds', methods=['POST'])
+# def info():
+#     payload = createJSON()
+#     return 'Done', 201
+
+# @main.route('/odds')
+# def info():
+#     payload = createJSON()
+#     return payload
 
 app = Flask(__name__)
 from getData import *
 
 @app.route("/")
+@app.route("/data")
+@app.route('/data', methods=['GET'])
 def info():
     payload = createJSON()
     return payload
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,3 +1,36 @@
+import React from "react";
+import './App.css';
+import axios from "axios";
+
+
+const baseURL = "http://localhost:5000/data";
+
+function App() {
+  const [post, setPost] = React.useState(null);
+
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(response.data);
+    });
+  }, []);
+
+  if (!post) return null;
+
+  return (
+    <div>
+      <h1>{post.fights}</h1>
+      <p>{post.oddsShark}</p>
+      
+    </div>
+  );
+}
+   
+export default App;
+
+
+
+
+
 // import logo from './logo.svg';
 // import './App.css';
 
@@ -18,35 +51,36 @@
 //           Learn React
 //         </a>
 //       </header>
+//     </div> 
+//   ); 
+// }
+
+// export default App;
+
+
+// import React, { useState, useEffect } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   const [currentTime, setCurrentTime] = useState(0);
+
+//   useEffect(() => {
+//     fetch('/').then(res => res.json()).then(data => {
+//       setCurrentTime(data);
+//     });
+//   }, []);
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+
+//         ... no changes in this part ...
+
+//         <p>The current time is {currentTime}.</p>
+//       </header>
 //     </div>
 //   );
 // }
 
 // export default App;
-
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  const [currentTime, setCurrentTime] = useState(0);
-
-  useEffect(() => {
-    fetch('/').then(res => res.json()).then(data => {
-      setCurrentTime(data);
-    });
-  }, []);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-
-        ... no changes in this part ...
-
-        <p>The current time is {currentTime}.</p>
-      </header>
-    </div>
-  );
-}
-
-export default App;
