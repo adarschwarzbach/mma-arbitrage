027b3eb11a85d4edc7e3d9f17787bf091b167305
Adar Schwarzbach <adar.schwarzbach@duke.edu>
2022-05-25 20:34:15 -0700

    added localhost5k to package

--- backend/main.py
+++ backend/main.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 from getData import *
 
 @app.route("/")
-def index():
+def info():
     payload = createJSON()
-    return jsonify(payload)
+    return payload
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    app.run(host="0.0.0.0", debug=True)
